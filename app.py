cba93f51351d3fd8b09a52d05d1f919030376171
Jeff Hodges <jeffhodges93@gmail.com>
2018-03-10 16:11:22 -0800

    Lots of preliminary code.

---
+++ app.py
@@ -0,0 +1,18 @@
+from bottle import run, post, request, response, route
+import os
+from dash_tools.dashd import *
+
+@route('/get_votes', method="post")
+def get_votes():
+    proposal_hash = request.forms.get("text")
+
+
+
+
+    package = {"response_type": "in_channel", "text": "{}".format(output_path)}
+    response.content_type = 'application/json'
+    return package
+
+if __name__ == '__main__':
+    port_config = int(os.getenv('PORT', 5000))
+    run(host='0.0.0.0', port=port_config)
